c610e6df6a7562d483d104445d4f775e38c5faee
Victoria Collier <victoriacollier@Victorias-MacBook-Pro.local>
2019-12-02 12:41:52 -0500

    adding php form

---
+++ php/data/contact.php
@@ -0,0 +1,41 @@
+<?php
+    if(empty($_POST)){
+    echo 'No...';
+    exit;
+    }
+
+    $name = '';
+    $email = '';
+    $subject = '';
+    $message = '';
+    $recipient = 'v.collier@hotmail.com';
+
+    //some validations
+    if(isset($_POST['name'])){
+        $name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+    }
+
+    if(isset($_POST['email'])){
+        $email = str_replace(array("\r", "\n", "%0a", "%0d"),'',$_POST['email']);
+        $email = filter_var($email, FILTER_VALIDATE_EMAIL);
+    }
+
+    if(isset($_POST['subject'])){
+        $subject = filter_var($_POST['subject'],FILTER_SANITIZE_STRING);
+    }
+
+    if(isset($_POST['message'])){
+        $message = $_POST['message'];
+    }
+
+    $headers = [
+    'From'=>'noreply@test.ca',
+    'Reply-To'=>$name.'<'.$email.'>'
+    ];
+
+    if(mail($recipient, $subject, $message, $headers)){
+        echo '<p>It sent! I will be reading your message as soon as possible. I promise tog et beck to you within two business days! Talk soon.</p>';
+    }else{
+        echo '<p>Oops. Someting went wrong, the message did not send. Try again shortly.</p>';
+    }
+?>
---
+++ php/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Victoria Collier - Contact Me</title>
+</head>
+<body>
+<form action="data/contact.php" method="post">
+    <h1>Welcome to my Contact Page. Feel free to...</h1>
+    <h2>REACH OUT</h2>
+
+    <label for="name-field">Name</label>
+    <input id="name-field" name="name" type="text" placeholder="Name">
+
+    <label for="email-field">Email</label>
+    <input id="email-field" name="email" type="email" placeholder="Email">
+
+    <label for="subject-field">Subject</label>
+    <input id="subject-field" name="subject" type="text" placeholder="Subject">
+
+    <label for="message-field">Message</label>
+    <textarea id="message-field" name="message"></textarea>
+    
+    <button type="submit">SEND IT!</button>
+</form>
+    
+</body>
+</html>
